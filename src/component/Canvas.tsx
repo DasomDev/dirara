13febe48ca2c716b09d2daf6bdc71eb412bb7c0e
DASOM CHO <dasomcho.dev@gmail.com>
2025-07-07 22:17:38 +0900

    React, Vite, TS setting

---
+++ src/component/Canvas.tsx
@@ -0,0 +1,20 @@
+import React, { useRef, useEffect } from 'react'
+
+const Canvas = () => {
+  const canvasRef = useRef<HTMLCanvasElement>(null)
+
+  useEffect(() => {
+    const canvas = canvasRef.current
+    if(!canvas) return
+
+    const ctx = canvas.getContext('2d')
+    if(!ctx) return
+
+    ctx.fillStyle = 'red'
+    ctx.fillRect(0, 0, 100, 100)
+  }, [])
+
+  return <canvas ref={canvasRef} />
+}
+
+export default Canvas 
